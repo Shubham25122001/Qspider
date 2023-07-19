4295c2be06baf558c21c07cdbafa8955b50ce44f
shubham <shubham25122001@gmail.com>
2023-07-19 22:57:16 +0530

    java increment Decrement

---
+++ java/19-07-2023/IncreDecre.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class IncreDecre{
+    public static void main(String[] args){
+        int x = -5;
+        int y = x++;
+        y = ++x + x++;
+        System.out.println("The value of x is : "+ x);
+        System.out.println("The value of y is :" + y);
+        
+    }
+}
